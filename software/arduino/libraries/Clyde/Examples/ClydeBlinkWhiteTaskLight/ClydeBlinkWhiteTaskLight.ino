99f7fe0c8dc959e7746793bb404a46f5b56609ef
angelagabereau <angela@fabule.com>
2014-10-24 19:07:55 -0400

    correct the comments in the blink task light example

--- software/arduino/libraries/Clyde/Examples/ClydeBlinkWhiteTaskLight/ClydeBlinkWhiteTaskLight.ino
+++ software/arduino/libraries/Clyde/Examples/ClydeBlinkWhiteTaskLight/ClydeBlinkWhiteTaskLight.ino
@@ -5,11 +5,10 @@
 #include <SoftwareSerial.h>
 #include <MPR121.h>
 
-/*
+/* 
  Clyde Blink White Task Light
- Turns on the white task light for one second, then off for one second, repeatedly.
-
- */
+ Turns on the white task light for one second, then off for one second, repeatedly. 
+*/
 
 // the setup routine runs once when you press reset:
 void setup() {                
@@ -21,14 +20,13 @@ void setup() {
 // the loop routine runs over and over again forever:
 void loop() {
 
-  //On
+  //OFF
   Clyde.setWhite(255);  // setting the white light to 255 turns it OFF.
   Clyde.updateWhiteLight();
-  delay(1000);  // wait for a second
+  delay(1000);  // wait for a second, or 1000 milliseconds
 
-  //Off
-  Clyde.setWhite(0); // setting the white light on turns it ON.
+  //On
+  Clyde.setWhite(0); // setting the white light to zero turns it ON.
   Clyde.updateWhiteLight();
-  delay(1000);  // wait for a second
+  delay(1000);  // wait for a second, or 1000 milliseconds
 }
-
